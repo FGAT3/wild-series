c345c9e76c42dc9812eb1adeff3815dad3167c05
FGAT3 <feliciengamelin@yahoo.fr>
2024-05-20 16:18:45 +0200

    router programs

--- server/index.js
+++ server/index.js
@@ -20,11 +20,3 @@ app
   .on("error", (err) => {
     console.error("Error:", err.message);
   });
-
-// Routes
-
-const sayHello = (req, res) => {
-  res.send("Welcome to Wild Series !");
-};
-
-app.get("/", sayHello);
